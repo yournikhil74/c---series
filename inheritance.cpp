659b837efede7f1350a51e59b4738aa948339e86
yournikhil74 <yournikhil74@gmail.com>
2025-11-19 16:25:48 +0530

    Add batch variable

--- inheritance.cpp
+++ inheritance.cpp
@@ -6,10 +6,11 @@ class person{
    public: 
     string name;
     string subject; 
-
-    person(string name,string subject){
+    string batch;
+    person(string name,string subject,string batch){      
         this->name=name;
         this->subject=subject;
+        this->batch=batch;
     }
     
 };
@@ -17,7 +18,7 @@ class student: public person{
     public:
     string rollnumber;
     
-    student(string name,string subject,string rollnumber):person(name,subject){
+    student(string name,string subject,string rollnumber,string batch):person(name,subject,batch){    //constructor//
     
         this->rollnumber=rollnumber; 
     }
@@ -27,11 +28,12 @@ class student: public person{
             cout<<"name is: "<<name<<endl;
             cout<<"subject is: "<<subject<<endl;
             cout<<"rollnumber is: "<<rollnumber<<endl;
+            cout<<"batch year: "<<batch<<endl;
         }
     
 };
 int main(){
-    student s1("nikhil","c++","87");
+    student s1("nikhil","c++","87","2024-2028");
     s1.getInfo();
     return 0;
 }
